dbd1488fa5ac1a8afdc557bd716862d360913265
Le Trong Dat <datlt2306@gmail.com>
2022-05-12 12:19:49 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sum22';
+  productName: string = "Product A";
+  productPrice: number = 10;
+  productStatus: boolean = false;
+  productInfo: { id: number, price: number, name: string } = {
+    id: 1,
+    name: "product A",
+    price: 200
+  };
+
+  productList: { id: number, price: number, name: string, status: boolean }[] = [
+    {
+      id: 1,
+      name: "product A",
+      price: 200,
+      status: false
+    },
+    {
+      id: 2,
+      name: "product B",
+      price: 300,
+      status: true
+    }
+  ]
 }
+
+/* 
+  productList.map(item => (
+    <div>
+
+    </div>
+  ))
+*/
