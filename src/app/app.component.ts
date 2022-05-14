0f88b86af9ff8bfb5a6d7b8b45c2e62385c878b4
Le Trong Dat <datlt2306@gmail.com>
2022-05-14 12:49:53 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,31 +21,4 @@ export class AppComponent {
       status: true
     }
   ]
-  // onHandleClick() {
-  //   this.productStatus = !this.productStatus
-  // }
-  // onHandleDelete(id: number) {
-  //   console.log(id);
-  //   this.productList = this.productList.filter(product => product.id !== id);
-  // }
-
-  // onHandleKeyPress(event: any) {
-  //   console.log(event.target.value);
-  //   this.title = event.target.value;
-  // }
-  // onSubmit() {
-  //   console.log('1')
-  // }
-  // onHandleGetInfo(product: IProduct) {
-  //   this.productDetail = product;
-  //   console.log('product', product)
-  // }
 }
-
-/* 
-  productList.map(item => (
-    <div>
-
-    </div>
-  ))
-*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './pages/about/about.component';
-import { HelloComponent } from './hello/hello.component';
+import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { ProductAddComponent } from './components/product-add/product-add.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent,
-    HelloComponent,
+    ProductComponent,
     ProductsComponent,
-    ProductDetailComponent,
-    ProductAddComponent
   ],
   imports: [
     BrowserModule,
