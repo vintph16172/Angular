d548c73daaf4ce70d8b05e8997ee15dd38c53191
Le Trong Dat <datlt2306@gmail.com>
2022-05-26 13:15:30 +0700

    update product

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from 'src/app/models/Product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -8,9 +9,10 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent implements OnInit {
-  product: { name: string, price: number } = {
+  product: IProduct = {
     name: "",
-    price: 0
+    price: 0,
+    status: true
   }
   constructor(
     private productService: ProductService,
@@ -27,12 +29,20 @@ export class ProductAddComponent implements OnInit {
     }
   }
   onSubmit() {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    if (id) {
+      this.productService.updateProduct(this.product).subscribe(data => {
+        setTimeout(() => {
+          // redirect về product list
+          this.router.navigateByUrl('/product');
+        }, 2000)
+      })
+    }
     this.productService.addProduct(this.product).subscribe(data => {
-      console.log('Thêm thành công');
       setTimeout(() => {
         // redirect về product list
         this.router.navigateByUrl('/product');
       }, 2000)
-    })
+    });
   }
 }
